6979366216b10c5b1a951cbc7fa8f01e7fb2b1c2
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-04-28 20:01:49 +0300

    добавил Регистрация модели позователя в Админ-панели

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Регистрация модели позователя в Админ-панели
+    """
+    list_display = ('id',  'id_chat', 'first_name', 'last_name', 'email', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('first_name', 'last_name',)
