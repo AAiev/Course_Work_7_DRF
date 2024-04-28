83e1a68b4a19b4d787c436a7aacb0d008452bb1e
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-04-28 19:55:17 +0300

    добавил вьюхи для создания, отображения и удаления пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    """
+    Контроллер создания сущности для модели Пользователя
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    """
+    Контроллер для просмотора конкретной сущности модели Пользователя
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    """
+    Контроллер для обновления сущности модели Пользователя
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDeleteAPIView(DestroyAPIView):
+    """
+    Контроллер для удаления сущности модели Пользователя
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
