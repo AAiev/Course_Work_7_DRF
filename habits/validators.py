06a33225dff8ed4f7f67242c8dcf94f31e4d255d
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-04-28 20:26:12 +0300

    добавил Валидация времени выполнения привычки.

--- habits/validators.py
+++ habits/validators.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ValidationError
 
+from services.constants import TIME_COMPLATE
 
 class HabitValidator:
     """
@@ -10,6 +11,23 @@ class HabitValidator:
         self.award = award
 
     def __call__(self, value):
-        if self.related_habit and self.award:
+        related_habit = value.get('related_habit')
+        award = value.get('award')
+        if related_habit and award:
             raise ValidationError("Запрещено выбирать односременно связанную привычку и вознаграждение")
 
+
+
+class TimeCompleteValidator:
+    """
+    Валидация времени выполнения привычки. не более TIME_COMPLATE секунд.
+    TIME_COMPLATE - время указывается в файле services/constants.py
+    """
+
+    def __init__(self, time_to_complete):
+        self.time_to_complete = time_to_complete
+
+    def __call__(self, value):
+        time_to_complete = value.get('time_to_complete')
+        if time_to_complete > TIME_COMPLATE:
+            raise ValidationError(f"Время выполнения привычки должно быть не больше {TIME_COMPLATE} секунд)")
