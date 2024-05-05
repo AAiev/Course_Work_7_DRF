c4a5ec7380dc13c5cf4bf5663dd317824dc9aa78
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-05-05 11:28:42 +0300

    добавил урлы для приложения Habits

---
+++ habits/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from habits.apps import HabitsConfig
+from habits.views import (HabitCreateAPIView, HabitListAPIView,
+                          HabitRetrieveAPIView, HabitUpdateAPIView, HabitDestroyAPIView)
+
+app_name = HabitsConfig.name
+
+urlpatterns = [
+    path('create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('', HabitListAPIView.as_view(), name='habit-list'),
+    path('<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit-detail'),
+    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('delete/<int:pk>', HabitDestroyAPIView.as_view(), name='habit-delete'),
+
+]
